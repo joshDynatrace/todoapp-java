e0c1e32411d7486b08a6a6ad3f7b2d8ac5de5065
joshDynatrace <joshua.hendrick@dynatrace.com>
2026-02-06 09:32:31 -0800

    Modify setup script to comment out Gradle installation
    Comment out Gradle installation steps and update script flow.

--- setup-debian.sh
+++ setup-debian.sh
@@ -6,14 +6,16 @@ sudo apt-get install -y git
 
 sudo apt install -y default-jdk
 
-sudo apt-get install curl unzip -y
-
-wget https://services.gradle.org/distributions/gradle-9.0.0-all.zip
-unzip gradle-9.0.0-all.zip
-sudo mv gradle-9.0.0 /opt/gradle
-echo "export PATH=/opt/gradle/bin:${PATH}" | sudo tee /etc/profile.d/gradle.sh
-sudo chmod +x /etc/profile.d/gradle.sh
-source /etc/profile.d/gradle.sh
+# sudo apt-get install curl unzip -y
+# wget https://services.gradle.org/distributions/gradle-9.0.0-all.zip
+# unzip gradle-9.0.0-all.zip
+# sudo mv gradle-9.0.0 /opt/gradle
+# echo "export PATH=/opt/gradle/bin:${PATH}" | sudo tee /etc/profile.d/gradle.sh
+# sudo chmod +x /etc/profile.d/gradle.sh
+# source /etc/profile.d/gradle.sh
 
 git clone https://github.com/joshDynatrace/todoapp-java
 cd todoapp-java
+
+./gradlew -i bootJar
+java -jar build/libs/tutorial-1.0.0.jar
