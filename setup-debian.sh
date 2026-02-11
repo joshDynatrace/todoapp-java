cf42e0e6d77674e1ee17e83d7f2cd57a8b8c5dcf
joshDynatrace <joshua.hendrick@dynatrace.com>
2026-02-10 16:37:03 -0800

    Comment out Dynatrace OneAgent deployment
    Comment out Dynatrace OneAgent deployment commands

--- setup-debian.sh
+++ setup-debian.sh
@@ -15,3 +15,7 @@ cd todoapp-java
 # Run the todo app
 ./gradlew -i bootJar
 java -jar build/libs/tutorial-1.0.0.jar
+
+# Deploy Dynatrace OneAgent
+# wget  -O Dynatrace-OneAgent-Linux-1.329.73.20260123-140641.sh "https://arn95277.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86" --header="Authorization: Api-Token $token"
+# sudo /bin/sh Dynatrace-OneAgent-Linux-1.329.73.20260123-140641.sh --set-monitoring-mode=discovery --set-app-log-content-access=true
